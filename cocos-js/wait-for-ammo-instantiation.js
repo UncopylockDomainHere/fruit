a25f20416ffc145c36493692806b9f41cdbfc26e
prokid8467-collab <prokid8467@gmail.com>
2026-04-04 05:57:34 -0400

    Update wait-for-ammo-instantiation.js

--- cocos-js/wait-for-ammo-instantiation.js
+++ cocos-js/wait-for-ammo-instantiation.js
@@ -1,4 +1,4 @@
-System.register(['./instantiated-27da64f9.js'], function (exports) {
+System.register(['https://cdn.jsdelivr.net/gh/UncopylockDomainHere/fruits/instantiated-27da64f9.js'], function (exports) {
 	'use strict';
 	return {
 		setters: [function (module) {
